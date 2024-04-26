cc411ed6ae0b09b8500fd885c89ffa4a6c323a56
unknown <juanrodriguezthc14@gmail.com>
2024-04-26 14:58:37 -0500

    entrega dos

--- app/src/main/java/com/example/tiendamusical/MainActivity.java
+++ app/src/main/java/com/example/tiendamusical/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.tiendamusical;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView15 = findViewById(R.id.textView15);
+        textView15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Iniciar la actividad de Registro al hacer clic en textView15
+                Intent intent = new Intent(MainActivity.this, Registro.class);
+                startActivity(intent);
+            }
+        });
     }
 }
